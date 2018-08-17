0eaca5b8d0880040dcfb9f05448b7a3a2f465da9
devinsg <duyanh2005@gmail.com>
2018-08-17 09:25:27 +0700

    img path

--- App.js
+++ App.js
@@ -6,13 +6,15 @@ export default class App extends React.Component {
     let pic = {
       uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
     };
+    let spImage = require('./images/sharepoint-hub-site.png');
 
     return (
       <View style={styles.container}>
+        <Text>First React-Native-App !</Text>
         <Image source={pic} style={{width: 193, height: 110}}/>
 
-        <Text>Hello world !</Text>
-        <Text>First React-Native-App !</Text>
+        <Text>SharePoint</Text>
+        <Image source={spImage} style={{width: 293, height: 210}}/>
         <Text>CopyRight@2017 - Author: Vo Duy Anh</Text>
       </View>
     );
