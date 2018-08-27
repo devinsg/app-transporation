6d006d707d6397bf3d68dc7d499e95096197bab8
devinsg <duyanh2005@gmail.com>
2018-08-27 16:51:36 +0700

    clean code

--- App.js
+++ App.js
@@ -3,8 +3,9 @@ import Sample from './component/sample';
 
 class App extends Component {
   render() {
+    let author = 'James Bond 007';
     return (
-      <Sample />
+      <Sample author={author}/>
     );
   }
 };
--- component/sample/index.js
+++ component/sample/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import { StyleSheet, Text, View, Image, AppRegistry } from 'react-native';
 
 const styles = StyleSheet.create({
@@ -9,25 +10,36 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+const spImage = require('../../images/sharepoint-hub-site.png');
 
 class Sample extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      picture: {
+        uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
+      }
+    }
+  }
+
   render() {
-    let pic = {
-      uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
-    };
-    let spImage = require('../../images/sharepoint-hub-site.png');
-    
+    let { author } = this.props;
+    let { picture } = this.state;
     return (
       <View style={styles.container}>
         <Text>First React-Native-App !</Text>
-        <Image source={pic} style={{width: 193, height: 110}}/>
+        <Image source={picture} style={{width: 193, height: 110}}/>
 
         <Text>SharePoint</Text>
         <Image source={spImage} style={{width: 293, height: 210}}/>
-        <Text>CopyRight@2017 - Author: Vo Duy Anh</Text>      
+        <Text>CopyRight@2017 - Author: {author}</Text>      
       </View>
     );
   }
-}
+};
+
+Sample.propTypes = {
+  author: PropTypes.string
+};
 
 export default Sample;
