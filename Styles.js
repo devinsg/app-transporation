3507c748d5006aa088e54906ea96f24679e6bf45
devinsg <duyanh2005@gmail.com>
2018-08-28 17:56:34 +0700

    move styles out

--- Styles.js
+++ Styles.js
@@ -5,10 +5,58 @@ const styles = StyleSheet.create({
         backgroundColor: '#F5FCFF',
         flex: 1,
         paddingTop: 40,
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    loginContainer: {
+        backgroundColor: '#F5FCFF',
+        flex: 1,
+        paddingTop: 40,
         alignItems: 'center'
     },
+    logo: {
+        width: 66,
+        height: 55
+    },
+    heading: {
+        fontSize: 30,
+        marginTop: 10
+    },
+    welcome: {
+        fontSize: 20,
+        textAlign: 'center',
+        margin: 10
+    },
+    input: {
+        height: 50,
+        width: 300,
+        marginTop: 10,
+        padding: 4,
+        fontSize: 18,
+        borderWidth: 1,
+        borderColor: '#48bbec',
+        borderRadius: 50,
+        textAlign: 'center'
+    },
+    button: {
+        height: 50,
+        width: 300,
+        backgroundColor: '#48BBEC',
+        alignItems: 'stretch',
+        marginTop: 10,
+        justifyContent: 'center'
+    },
+    buttonText: {
+        fontSize: 22,
+        color: '#FFF',
+        alignSelf: 'center'
+    },
     loader: {
-        
+        marginTop: 20
+    },
+    error: {
+        color: 'red',
+        paddingTop: 10
     }
 });
 
