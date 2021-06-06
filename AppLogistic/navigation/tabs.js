c0038d5af1424e24638617d1f824b0587fe318cb
anhvoduy <voduyanh1984@yahoo.com>
2021-06-06 12:58:30 +0700

    - fix & clean up

--- AppLogistic/navigation/tabs.js
+++ AppLogistic/navigation/tabs.js
@@ -6,8 +6,7 @@ import {
   BottomTabBar,
 } from '@react-navigation/bottom-tabs';
 
-import Svg, { Path } from 'react-native-svg';
-//import { isIphoneX } from 'react-native-iphone-x-helper';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 import {Home, Order} from '../screens';
 import {COLORS, icons} from '../constants';
@@ -15,35 +14,18 @@ import {COLORS, icons} from '../constants';
 const Tab = createBottomTabNavigator();
 
 const Tabs = () => {
-  let logo = require('../assets/icons/cutlery.png');
   return (
     <Tab.Navigator
       tabBarOptions={{
         showLabel: true,
-        style: {
-          position: 'absolute',
-          left: 0,
-          bottom: 0,
-          right: 0,
-          borderTopWidth: 0,
-          backgroundColor: 'transparent',
-          elevation: 0,
-        },
       }}>
       <Tab.Screen
         name="Home"
         component={Home}
         options={{
-          tabBarIcon: ({focused}) => {
-            <Image
-              source={logo}
-              resizeMode="contain"
-              style={{
-                width: 25,
-                height: 25,
-                tintColor: focused ? COLORS.primary : COLORS.secondary,
-              }}
-            />;
+          tabBarIcon: ({focused, color, size}) => {
+            let iconName = 'ios-information-circle';
+            return <Ionicons name={iconName} size={size} color={color} />;
           },
         }}
       />
